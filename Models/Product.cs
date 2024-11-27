9411eec80f8554586bb96a5aa350597c32e0cc25
Matheus-Pedro <matheus.caprioli.pedro@gmail.com>
2024-11-27 14:08:36 -0300

    :sparkles: feat(cart): Adiciona contagem de itens no carrinho e novo modelo de produto.

---
+++ Models/Product.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ecommerce_creator.Models
+{
+    public class Product
+    {
+        public int MyProperty { get; set; }
+    }
+}
---
+++ postgresql-database/README.md
@@ -0,0 +1,5 @@
+# Resolvendo o problema com o PG Admin
+Rode o seguinte comando:
+```bash
+sudo chown -R 5050:5050 ./data/pgadmin
+```
